8bb9926c51f5ebeb597d5616c345a13a261f1a90
jeremiandrade <jeremiandrade04@gmail.com>
2026-07-04 17:45:36 +0200

    Add movie routes for fetching all movies and individual movie details

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const connection = require('./database/connection')
 
 
 
@@ -22,3 +23,32 @@ app.listen(PORT, () => {
 //Colleghiamo l’app al db e verifichiamo che tutto funzioni
 //Prepariamo una rotta index per ottenere la lista dei film
 //Prepariamo una rotta show per ottenere i dettagli di un singolo film e le sue recensioni
+
+//route for all movies-INDEX
+
+
+app.get('/movies', (req, res) => {
+    res.send('all movies')
+})
+
+
+// route for movies ID-SHOW
+
+app.get('/movies/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    res.send(`select movies with ID:${id}`)
+})
+
+
+
+// app.get('/movies', (req, res) => {
+//     //preparo la query
+//     const sql = 'SELECT * FROM MOVIES'
+
+//     // eseguo la query
+//     connection.query(sql, (err, results) => {
+//         if (err) return res.status(500).json({ error: 'Database query failed' })
+//         res.json(results)
+//     })
+
+// })
